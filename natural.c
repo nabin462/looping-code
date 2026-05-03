d3f56de5637bf2cc0c3178555ccad9bdfe22d39d
nabin462 <budhathokinabin700@gmail.com>
2026-05-03 09:23:11 +0545

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,13 @@
+//WAP to display first n natural number
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\t",i);
+	}
+	return 0;
+}
