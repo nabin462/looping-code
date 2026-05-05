330c20ba41b49f95bbdd65ef56aa88d2b1118008
nabin462 <budhathokinabin700@gmail.com>
2026-05-05 10:59:46 +0545

    Add files via upload

---
+++ odd n num sum.c
@@ -0,0 +1,17 @@
+//WAP to display sum of n odd number
+#include<stdio.h>
+int main()
+{
+	int i,n,sum=0;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(i%2!=0)
+		{
+		sum=sum+i;
+		}
+	}
+	printf("sum=%d",sum);
+	return 0;
+}
