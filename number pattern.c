6b96d9b69852eae4de32dea113dc671990ee3cf4
nabin462 <budhathokinabin700@gmail.com>
2026-05-10 19:45:28 +0545

    Add files via upload

---
+++ number pattern.c
@@ -0,0 +1,15 @@
+//WAP to print pattern of number
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d\t",j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
