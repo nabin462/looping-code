e65559adc03f47e08cec8718bc51350c99eb7fb5
nabin462 <budhathokinabin700@gmail.com>
2026-06-19 15:11:45 +0545

    Add files via upload

---
+++ goto statment.c
@@ -0,0 +1,24 @@
+//goto statment use for print the even number
+#include<stdio.h>
+int main(){
+	int i,n;
+		
+		printf("enter the number\n");
+		scanf("%d",&n);
+			if(n>0){
+			
+				goto even;
+		}
+		
+		even:
+			printf("the even number are\n");
+			for(i=1;i<=n;i++){
+			if(i%2==0){
+			
+			{
+				printf("%d\n",i);
+			}
+	}
+}
+				return 0;
+}
