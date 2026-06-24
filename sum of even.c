455b2cf4cd3e86e3bd1230b7f4d33a4ebbd18080
nabin462 <budhathokinabin700@gmail.com>
2026-06-24 16:52:06 +0545

    Add files via upload

---
+++ sum of even.c
@@ -0,0 +1,16 @@
+// sum of n even number
+#include<stdio.h>
+int main(){
+	int i,n,sum=0;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		if(i%2==0){
+			sum=sum+i;
+			
+		}
+	}
+	printf("sum =%d",sum);
+	return 0;
+}
