02a0c90aea7e883463ec10730736a53903fbeb70
nabin462 <budhathokinabin700@gmail.com>
2026-06-26 14:32:58 +0545

    Add files via upload

---
+++ n odd num.c
@@ -0,0 +1,16 @@
+//WAP to print the  n odd number
+#include<stdio.h>
+int main(){
+	int i,n;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	printf("the odd number are\n");
+	for(i=1;i<=n;i++)
+	{
+		if(i%2!=0){
+		printf("%d\n",i);
+	}
+		
+	}
+	return 0;
+}
